765b94feda17761d127b5f3cda9a3bc0deca2bbd
App Generator <51070104+app-generator@users.noreply.github.com>
2024-06-03 16:13:28 +0300

    Remove unused APP (docs1)

--- apps/docs1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import serve_docs_index
-
-urlpatterns = [
-    path('', serve_docs_index, name='docs_index'),
-    # path('<path:path>/', serve_docs, name='docs_serve'),
-]
