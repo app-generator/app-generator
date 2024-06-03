3d4456c4028c5f783feea0a5e2521e44919b4fa2
MySecondLanguage <mmamunism@gmail.com>
2024-06-03 13:22:12 +0600

    added logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,9 @@ author = 'appseed'
 
 extensions = [
     'recommonmark',
+    'sphinx.ext.viewcode',
+    # 'myst_parser',
+    # 'sphinx_copybutton',
 ]
 
 templates_path = ['_templates']
@@ -25,5 +28,23 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
+
+
 html_static_path = ['_static']
+
+html_logo = '_static/logo-text.png'
+
+
+html_theme_options = {
+    "light_logo": "logo-text.png",
+    "dark_logo": "logo-text.png",
+    # "sidebar_hide_name": True,
+    # "navigation_with_keys": True,
+    # "top_of_page_buttons": ["view", "edit"],
+    # "announcement": "We will launch the stable version on July 1st, 2024. We are seeking your feedback",
+    # "source_repository": "https://github.com/nxtbn-com/nxtbn-docs",
+    # "source_branch": "main",
+    # "source_directory": "source",
+}
