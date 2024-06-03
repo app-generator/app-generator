3e9cf000aeacfa5f1a3545eec7a45b40bba9d020
App Generator <51070104+app-generator@users.noreply.github.com>
2024-06-03 13:03:38 +0300

    Integrate SEO Inputs

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -14,7 +14,15 @@ def index(request):
   func_name  = sys._getframe().f_code.co_name 
   logger( f'[{__name__}->{func_name}(), L:{currentframe().f_lineno}] ' + 'Begin' )
 
-  return render(request, 'pages/home.html')
+  context = {
+    'segment'        : 'home',
+    'page_title'     : 'Generate Code, AI-Tools, Deployment Automation, and Custom Development Services',
+    'page_info'      : 'Modern tools for developers and Companies, Generated Digital Products (Dashboards, eCommerce, Websites)',
+    'page_keywords'  : 'app generator, dashboards, web apps, generated products, custom development, ai tools, dev tools, tools for developers and companies',
+    'page_canonical' : '',
+  }
+
+  return render(request, 'pages/home.html', context)
 
 def support(request):
 
@@ -23,8 +31,13 @@ def support(request):
   logger( f'[{__name__}->{func_name}(), L:{currentframe().f_lineno}] ' + 'Begin' )
 
   context = {
-    'segment': 'support'
+    'segment'        : 'support',
+    'page_title'     : 'Premium Support via email (support@appseed.us) and Discord',
+    'page_info'      : 'Get Support for Dashboards, eCommerce, Presentation Websites',
+    'page_keywords'  : 'support, email support, Discord support, Tickets, app generator, dashboards, web apps, generated products, custom development',
+    'page_canonical' : 'support/',
   }
+
   return render(request, 'pages/support.html', context)
 
 def custom_development(request):
@@ -34,8 +47,13 @@ def custom_development(request):
   logger( f'[{__name__}->{func_name}(), L:{currentframe().f_lineno}] ' + 'Begin' )
 
   context = {
-    'segment': 'custom_development'
+    'segment'        : 'custom_development',
+    'page_title'     : 'Custom Development - Need help Coding an MVP or a Complete Solution? ',
+    'page_info'      : 'Get Custom Development Services from a team of experts.',
+    'page_keywords'  : 'custom development, custom tools, custom generators, app generator, dashboards, web apps, generated products',
+    'page_canonical' : 'custom-development/',
   }
+
   return render(request, 'pages/custom-development.html', context)
 
 def terms(request):
@@ -45,8 +63,13 @@ def terms(request):
   logger( f'[{__name__}->{func_name}(), L:{currentframe().f_lineno}] ' + 'Begin' )
 
   context = {
-    'segment': 'terms'
+    'segment'        : 'terms',
+    'page_title'     : 'Terms - Learn how to use the service ',
+    'page_info'      : 'AppSee/App-generator Terms of Use',
+    'page_keywords'  : 'terms, service terms, AppSeed terms, App-Generator Terms',
+    'page_canonical' : 'terms/',
   }
+
   return render(request, 'pages/terms.html', context)
 
 def about(request):
@@ -56,6 +79,11 @@ def about(request):
   logger( f'[{__name__}->{func_name}(), L:{currentframe().f_lineno}] ' + 'Begin' )
 
   context = {
-    'segment': 'about'
+    'segment'        : 'about',
+    'page_title'     : 'About US - Learn more about the team and the mission ',
+    'page_info'      : 'More inputs regarding AppSee/App-generator service',
+    'page_keywords'  : 'about, about AppSeed, about App-Generator',
+    'page_canonical' : 'about/',
   }
+
   return render(request, 'pages/about.html', context)
