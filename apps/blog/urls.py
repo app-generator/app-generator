8d4922a2ca4f939aa4c3055a5f8f98e2eef40a12
Mominur Rahman <mominur.rahman@heliosinsider.com>
2024-06-03 16:53:05 +0600

    Add tag filters in blog

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.utils.translation import gettext_lazy as _
 
 from . import views
 
 urlpatterns = [
-    path("blog/", views.blogs, name="blog"),
-    path("blog/<str:slug>/", views.blog_details, name="blog_detail"),
+    re_path(r'^blog/(?:(?P<tags>[^/]+)/)?$', views.blogs, name='blog'),
+    path("blog-detail/<str:slug>/", views.blog_details, name="blog_detail"),
     path("blog/tag/<str:slug>/", views.filter_by_tags, name="filter_by_tags"),
     path("add-bookmark/<str:slug>/", views.add_bookmark, name="add_bookmark"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -10,17 +10,26 @@ from django.core.paginator import Paginator
 from apps.blog.forms import ArticleForm
 from apps.common.models import State, Tag, Article, Bookmark, File, FileType
 
-def blogs(request):
+def blogs(request, tags=None):
     page = request.GET.get('page', 1)
     search_query = request.GET.get('search', '')
     
-    articles = Article.objects.filter(title__icontains=search_query, state=State.PUBLISHED).order_by('-published_at')
+    articles = Article.objects.filter(title__icontains=search_query, state=State.PUBLISHED)
+    tag_list = []
+    if tags:
+        tag_list = tags.split(',')
+        articles = articles.filter(tags__slug__in=tag_list).distinct()
+    
+    articles = articles.order_by('-published_at')
     paginator = Paginator(articles, per_page=12)
+    tags = Tag.objects.all()
 
     context = {
         'articles': paginator.get_page(page),
         'total_pages': paginator.num_pages,
-        'segment': 'blog'
+        'segment': 'blog',
+        'tags': tags,
+        'tag_list': tag_list
     }
 
     return render(request, 'pages/blogs/index.html', context)
