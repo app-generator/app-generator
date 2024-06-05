262e513e54419c836ad1eeacadc6cf4d68ae6294
Mominur Rahman <mominur.rahman@heliosinsider.com>
2024-06-05 15:25:24 +0600

    Clean up home page and add termino

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product-dashboard/", views.show_dashboard, name="show_dashboard"),
     path("support/", views.support, name="support"),
     path("custom-development/", views.custom_development, name="custom_development"),
     path("terms/", views.terms, name="terms"),
