5f4de7274b2906514301e6ee8a80c788003919ad
Mominur Rahman <mominur.rahman@heliosinsider.com>
2024-06-09 15:06:54 +0600

    Add product tags filter

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from apps.common.models_products import Products, Type
+from apps.common.models_products import Products, Type, Tech1, Tech2, CssSystem, DesignSystem
 from django.db.models import Count
 from django.db.models import QuerySet
+from django.db.models import Q
 
 # Create your views here.
 
 
-def products_view(request):
+def get_filtered_choices(choices):
+    return [{'value': choice[0], 'label': choice[1]} for choice in choices if choice[0] != 'NA']
+
+def get_values(choices):
+    return [choice[0] for choice in choices if choice[0]!= 'NA']
+
+def products_view(request, tags=None):
     filter_string = {}
     if search := request.GET.get('search'):
         filter_string['name__icontains'] = search
@@ -14,6 +21,23 @@ def products_view(request):
     products = Products.objects.filter(**filter_string)
     grouped_products = {}
 
+    tag_list = []
+    if tags:
+        tag_list = tags.split(',')
+    
+    tag_filters = Q()
+    for tag in tag_list:
+        if tag in get_values(Tech1.choices):
+            tag_filters |= Q(tech1=tag)
+        elif tag in get_values(Tech2.choices):
+            tag_filters |= Q(tech2=tag)
+        elif tag in get_values(CssSystem.choices):
+            tag_filters |= Q(design_css=tag)
+        elif tag in get_values(DesignSystem.choices):
+            tag_filters |= Q(design_system=tag)
+
+    products = products.filter(tag_filters)
+    
     for product in products:
         tech1 = product.tech1
 
@@ -22,11 +46,20 @@ def products_view(request):
 
         grouped_products[tech1].append(product)
 
+    combined_choices = {
+        'tech1': get_filtered_choices(Tech1.choices),
+        'tech2': get_filtered_choices(Tech2.choices),
+        'css_system': get_filtered_choices(CssSystem.choices),
+        'design_system': get_filtered_choices(DesignSystem.choices),
+    }
+
     context = {
         'grouped_products': grouped_products,
         'page_title': 'Free and PAID starters but with Djang0, Flask, Node, and React',
         'page_info': 'Production-ready starters crafted by AppSeed.',
-        'page_keywords': 'django, starters, flask, node, react'
+        'page_keywords': 'django, starters, flask, node, react',
+        'combined_choices': combined_choices,
+        'tag_list': tag_list
     }
     return render(request, 'pages/products/index.html', context)
 
