c317c9799e41d80d661c1e99ea968a2c473373d1
App Generator <51070104+app-generator@users.noreply.github.com>
2024-09-11 11:42:50 +0300

    Create views.py

---
+++ apps/tools/views.py
@@ -0,0 +1,11 @@
+import requests, base64
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.http import HttpResponse
+from django.utils.safestring import mark_safe
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('tools ' + '<a href="/tools/db-editor">DB Editor</a>')
