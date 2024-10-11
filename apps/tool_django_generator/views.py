f0c3b863c80ea0b5fd9088f4388c525af7141650
ar-rehman135 <abdulrehmanmirza135@gmail.com>
2024-10-11 12:49:53 +0500

    Update backend

--- apps/tool_django_generator/views.py
+++ apps/tool_django_generator/views.py
@@ -4,8 +4,18 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, "tools/db-editor.html")
+    return render(request, "tools/django-generator.html")
+
+
+class StatusView(APIView):
+    def post(self, request):
+        response_data = {"status": "200", "info": "DJango Template is generating "}
+        return Response(response_data, status=status.HTTP_200_OK)
