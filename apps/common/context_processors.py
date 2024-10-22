3e41ea9dee88135d22cb230e01d0f8b0745323be
mominur-helios <mominur.rahman@heliosinsider.com>
2024-10-22 22:46:54 +0600

    Add version

--- apps/common/context_processors.py
+++ apps/common/context_processors.py
@@ -1,7 +1,14 @@
 from apps.common.models_authentication import Profile, RoleChoices
+from django.conf import settings
 
 def profile_context(request):
     is_company = False
     if request.user.is_authenticated:
         is_company = Profile.objects.filter(user=request.user, role=RoleChoices.COMPANY).exists()
     return {'is_company': is_company}
+
+
+def version_context(request):
+    return {
+        'version': getattr(settings, 'VERSION')
+    }
