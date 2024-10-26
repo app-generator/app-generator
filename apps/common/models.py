78f073da988210d2bfed0060a710b54cc1406168
mominur-helios <mominur.rahman@heliosinsider.com>
2024-10-26 18:12:41 +0600

    Capture 500 error event

--- apps/common/models.py
+++ apps/common/models.py
@@ -26,7 +26,7 @@ class EventType(models.TextChoices):
 class Event(BaseModel):
     userId = models.IntegerField(default=-1)
     type = models.CharField(max_length=100, choices=EventType.choices, default=EventType.GENERAL)
-    text = models.CharField(max_length=512, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.userId}, {self.type} -> {self.text}"
