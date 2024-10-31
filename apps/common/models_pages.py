ba78d94fdbaabd5202caf0ab30da7b1314400d2f
mominur-helios <mominur.rahman@heliosinsider.com>
2024-10-31 22:39:21 +0600

    Functional custom development form

--- apps/common/models_pages.py
+++ apps/common/models_pages.py
@@ -15,3 +15,22 @@ class Prompt(models.Model):
     user_id = models.CharField(max_length=10)
     question = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProjectTypeChoices(models.TextChoices):
+    CUSTOM_SOFTWARE_DEVELOPMENT = 'CUSTOM_SOFTWARE_DEVELOPMENT', 'Custom Software Development'
+    SERVICE_OPTIMIZATION = 'SERVICE_OPTIMIZATION', 'Service Optimization'
+    CLOUDE_DEPLOYMENT = 'CLOUDE_DEPLOYMENT', 'Cloud Deployment (Digital Ocean, AWS, Azure)'
+
+class BudgetRangeChoices(models.TextChoices):
+    UNDER_5k = 'UNDER_5k', 'Under $5000'
+    F5K_T10k = 'F5K_T10k', '$5000 - $10,000'
+    OVER_10k = 'OVER_10k', 'Over $10000'
+
+class CustomDevelopment(BaseModel):
+    project_type = models.CharField(max_length=255, choices=ProjectTypeChoices.choices)
+    budget_range = models.CharField(max_length=100, choices=BudgetRangeChoices.choices)
+    name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
