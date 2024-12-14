2b67e2b767c02c8f45f47035d5b511a53dcbc7af
App Generator <51070104+app-generator@users.noreply.github.com>
2024-12-14 14:01:58 +0200

    Update gunicorn-cfg.py

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,8 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 bind = '0.0.0.0:5005'
-workers = 1
+workers = 10
 accesslog = '-'
-loglevel = 'debug'
+loglevel = 'error'
 capture_output = True
 enable_stdio_inheritance = True
