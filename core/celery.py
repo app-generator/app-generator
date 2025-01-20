95c0de36345a2e61d68fd23c678358d97651fae3
App Generator <51070104+app-generator@users.noreply.github.com>
2025-01-20 09:22:14 +0200

    Celery - Disable Maintenance Tasks

--- core/celery.py
+++ core/celery.py
@@ -11,24 +11,34 @@ celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
 
 celery_app.conf.beat_schedule = {
+    '''
     'run-critical-task': {
         'task': 'apps.tasks.tasks.run_critical_task',
         'schedule': crontab(minute='*/5'),
     },
+    '''
+    '''
     'run-hourly-task': {
         'task': 'apps.tasks.tasks.run_hourly_task',
         'schedule': crontab(minute=0, hour='*/1'),
     },
+    '''
+    '''
     'run-daily-task': {
         'task': 'apps.tasks.tasks.run_daily_task',
         'schedule': crontab(minute=0, hour=0),
     },
+    '''
+    '''
     'run-weekly-task': {
         'task': 'apps.tasks.tasks.run_weekly_task',
         'schedule': crontab(minute=0, hour=0, day_of_week='sunday'),
     },
+    '''
+    '''
     'run-monthly-task': {
         'task': 'apps.tasks.tasks.run_monthly_task',
         'schedule': crontab(minute=0, hour=0, day_of_month='1'),
     },
+    '''
 }
