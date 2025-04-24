29aced40e7ea0ff16edfb41ef6c9f9cb02f6f6c5
App Generator <51070104+app-generator@users.noreply.github.com>
2025-04-24 13:39:48 +0300

    Dummy - Update Card Helper

--- apps/dashboard/templatetags/custom_tags.py
+++ apps/dashboard/templatetags/custom_tags.py
@@ -100,6 +100,43 @@ def info_value(path):
         return file_info.first().info
     else:
         return ""
+
+@register.filter
+def h_product_card_info(aProdCanonical):
+    
+    product = Products.objects.filter(canonical=aProdCanonical).first()
+
+    if not product:
+        return '' #f"aProdCanonical '{aProdCanonical}' not found in DB"
+    
+    prod_card = f'''
+                <div class="md:col-span-1 col-span-2 mb-5">
+                  <div class="relative bg-white shadow-lg rounded-lg group mb-5">
+                      <img src="/static/{ product.canonical }top.png" alt="{ product.seo_description }" class="rounded-lg">
+                      <div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg"></div>
+                      <button 
+                          class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                          <a href="{ product.url_demo }" target="_blank" class="px-4 py-2 bg-white text-gray-900 font-medium rounded ">
+                              Demo
+                          </a>
+                      </button>
+                  </div>
+                  <div class="px-3">
+                      <div class="flex justify-between items-center">
+                          <h2 class="text-xl text-black font-medium">
+                            <a target="_blank" href="/{ product.canonical }">{ product.name }</a>
+                          </h2>
+                          <p class="text-gray-700">
+                              <span class="text-blue-600">${ product.price }</span>
+                          </p>
+                      </div>
+                      <p class="text-gray-700">
+                          { product.card_info } 
+                      </p>
+                  </div>
+                </div>
+    '''
+    return prod_card
     
 @register.filter
 def h_product_card(aProdCanonical):
