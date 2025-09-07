1eb6b802d3d2936ad1a788108b7060e0e85e0ff3
Mominur Rahman <mominurrohman774@gmail.com>
2025-09-07 23:09:14 +0600

    Restrict content for guest users

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -113,6 +113,14 @@ class ArticleForm(forms.Form):
         }),
         required=False
     )
+    summary = QuillFormField(
+        label=_("Summary"),
+        widget=forms.Textarea(
+            attrs={
+            'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 
+            'placeholder': 'Type here',
+        }),
+    )
     content = QuillFormField(
         label=_("Content"),
         widget=forms.Textarea(
