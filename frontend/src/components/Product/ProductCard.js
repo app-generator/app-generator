5c44f5258e86e447d578dc98c598912d800ac063
App Generator <app-generator@appseed.us>
2025-09-23 15:38:47 +0300

    Update ProductCard.js

--- frontend/src/components/Product/ProductCard.js
+++ frontend/src/components/Product/ProductCard.js
@@ -45,7 +45,7 @@ const ProductCard = ({ product, onClickFunc, productId }) => {
                 </a>
                 <button
                     onClick={() => onClickFunc(product.id)}
-                    className="inline-flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    className="gumroad inline-flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                     Get Discount
                 </button>
