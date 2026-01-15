766fcc128aacbc8c2d7932308d04c464325d8408
App Generator <app-generator@appseed.us>
2026-01-15 10:13:46 +0200

    Update StatusModal.js

--- frontend/src/django-generator/StatusModal.js
+++ frontend/src/django-generator/StatusModal.js
@@ -57,19 +57,6 @@ export function Status({ open, handleClose, status, isError }) {
                         </center>
                     )}
                 </Modal.Footer>
-                {!isError &&
-                <Modal.Footer>
-                    <div className='w-full flex justify-center items-center flex-col'>
-                        <p className='mb-2 text-center'>
-                            <a className='text-gray-600 dark:text-gray-200' href="">Get Hosting - 2.99/mo</a>
-                        </p>
-                        <a className='text-center' href="#">
-                            <img 
-                                className='w-16 h-14'
-                                src="https://d7h9v39iheghu.cloudfront.net/previews/da691f97-99cf-45a2-885d-1e5357ee3d49/3dddef1b-88fb-4f8d-9466-4de63fb3cc5d/3dddef1b-88fb-4f8d-9466-4de63fb3cc5d/1952x1000.jpg?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vZDdoOXYzOWloZWdodS5jbG91ZGZyb250Lm5ldC9wcmV2aWV3cy9kYTY5MWY5Ny05OWNmLTQ1YTItODg1ZC0xZTUzNTdlZTNkNDkqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzYwNDk5OTIzfX19XX0_&Signature=Sr9DFt4X2uMk6I1BLWRU8zZqIQxtPRSLXq15uUxs6uSPTTC0k-XkcDI7NTl7Sm~g68qm1u2jBNALTRnEHXJr-5LoEM8TwdnJ0sLts8BF8XEY7hlrQdeNxOGGZ5j0ViKRtdGssJLoFYkyqbRNhcl5l5t0UldNAudBG77MK~QIt~mN5SSNdky3HasJI6WXY7gH7KLfBcZkh9H7SiQzjJY0WAS6ZJqX8iPuzAhOToU6sVdymhj-81RW5RIKOTdU-LY3TVxMjEPvc~FANrR6UxKg9QHydkat3fdEA7SUHtS2U3XQxycr-XjLhgVZdYcfGQEdq~6tK55KOWXS0dvq4-Wiww__&Key-Pair-Id=APKAJXJN6VNR3OLZJXJA" alt="" />
-                        </a>
-                    </div>
-                </Modal.Footer>}
             </Modal>
         </>
     );
